d4d37179c3ef28e1c0e530811775d77f88ebbf90
SoloRus-Dev <rus.solovey.09@mail.ru>
2025-08-17 07:53:20 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
-#include <cstdlib> // Для getenv
-
-int main() {
-    const char* path = std::getenv("PATH");
-    if (path != nullptr) {
-        std::cout << "PATH: " << path << std::endl;
-    } else {
-        std::cout << "PATH environment variable not found." << std::endl;
+#include <fstream>
+#include <string>
+ 
+int main()
+{
+    std::string line;
+    std::string fileName ("text.txt");
+ 
+    std::ifstream in(fileName);
+    if (in.is_open())
+    {
+        while (std::getline(in, line))
+        {
+            std::cout << line << std::endl;
+        }
     }
-    std::cout << "Hello, world!" << std::endl;
+    in.close();
 
-    // Добавляем паузу, чтобы консоль не закрывалась сразу
+    std::ofstream out(fileName, std::ios::app);
+    if (out.is_open())
+    {
+        out << "Welcome to C++" << std::endl;
+    }
+    out.close();   
+
     std::cout << "Press any key to continue..." << std::endl;
-    std::cin.get(); // Ждем ввода символа
+    std::cin.get();
 
     return 0;
 }
