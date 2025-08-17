095f75817d394e34d5d6ad68bd8a9cb17fabcf32
SoloRus-Dev <rus.solovey.09@mail.ru>
2025-08-17 18:42:19 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -4,7 +4,25 @@
 #include <ncurses/ncurses.h>
 
 int main() {
-  initscr(); 
+  initscr();
+
+  if (has_colors()) {
+    start_color();
+
+    short color_cream = 10;
+    init_color(color_cream,
+              255 * 0.95, 255 * 0.9,
+              255 * 0.8);
+
+    init_pair(1, color_cream, COLOR_BLACK);
+
+    bkgd(COLOR_PAIR(1));
+    attron(COLOR_PAIR(1));
+  }
+  else {
+    printw("Your terminal does not support colors.\n");
+  }
+
   cbreak();
   noecho();
 
@@ -17,22 +35,23 @@ int main() {
   if (in.is_open()) {
     int row = 1;
     while (std::getline(in, line)) {
-      printw((std::to_string(row) + ".").c_str()); // Выводим номер строки с точкой
-      printw(" "); // Добавляем пробел после номера строки
-      printw(line.c_str()); // Выводим строку
-      printw("\n"); // Переходим на новую строку
+      printw((std::to_string(row) + ".").c_str());
+      printw(" ");
+      printw(line.c_str());
+      printw("\n");
       row++;
     }
   }
   in.close();
 
-  //std::ofstream out(fileName, std::ios::app);
-  //if (out.is_open()) {
+  // std::ofstream out(fileName, std::ios::app);
+  // if (out.is_open()) {
   //  out << "Welcome to C++" << std::endl;
-  //}
-  //out.close();
+  // }
+  // out.close();
 
   printw("\n\nPress any key to continue...\n");
+  attroff(COLOR_PAIR(1));
   getch();
   endwin();
   return 0;
