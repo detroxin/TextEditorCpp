57b119a7ebb50017fd2109bc943fafb70e7d6319
SoloRus-Dev <rus.solovey.09@mail.ru>
2025-08-17 12:00:42 +0300

    Update (ncurses add)

--- main.cpp
+++ main.cpp
@@ -1,31 +1,16 @@
-#include <iostream>
-#include <fstream>
-#include <string>
- 
-int main()
+#include <ncurses/ncurses.h>
+
+int main(int argc, char ** argv)
 {
-    std::string line;
-    std::string fileName ("text.txt");
- 
-    std::ifstream in(fileName);
-    if (in.is_open())
-    {
-        while (std::getline(in, line))
-        {
-            std::cout << line << std::endl;
-        }
-    }
-    in.close();
-
-    std::ofstream out(fileName, std::ios::app);
-    if (out.is_open())
-    {
-        out << "Welcome to C++" << std::endl;
-    }
-    out.close();   
-
-    std::cout << "Press any key to continue..." << std::endl;
-    std::cin.get();
-
+    // init screen and sets up screen
+    initscr();
+    // print to screen
+    printw("Hello World");
+    // refreshes the screen
+    refresh();
+    // pause the screen output
+    getch();
+    // deallocates memory and ends ncurses
+    // endwin();
     return 0;
 }
