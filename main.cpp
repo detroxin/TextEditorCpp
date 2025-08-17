f90ba1987539de3dbded826aae4b27ec7630950d
SoloRus-Dev <rus.solovey.09@mail.ru>
2025-08-17 18:19:21 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -11,12 +11,16 @@ int main() {
   std::string line;
   std::string fileName("text.txt");
 
+  printw("My terminal supports %d colors.\n", COLORS);
+
   std::ifstream in(fileName);
   if (in.is_open()) {
     int row = 1;
     while (std::getline(in, line)) {
-      mvprintw(row - 1, 0, (std::to_string(row) + ".").c_str());
-      mvprintw(row - 1, 4, line.c_str());
+      printw((std::to_string(row) + ".").c_str()); // Выводим номер строки с точкой
+      printw(" "); // Добавляем пробел после номера строки
+      printw(line.c_str()); // Выводим строку
+      printw("\n"); // Переходим на новую строку
       row++;
     }
   }
@@ -28,7 +32,7 @@ int main() {
   //}
   //out.close();
 
-  mvprintw(LINES - 1, 0, "Press any key to continue...");
+  printw("\n\nPress any key to continue...\n");
   getch();
   endwin();
   return 0;
